4e36da04396bd288ba35b583e4d250046bcd45a8
slagroom <sl4gr00m@gmail.com>
2019-12-22 22:03:52 -0400

    day 10 python

--- 10/python/main.py
+++ 10/python/main.py
@@ -1,18 +1,63 @@
+from collections import defaultdict
+import math
 import sys
-
-def reverse(iter):
-    it = []
-    for x in iter:
-        it.append(x)
-    return reversed(it)
 
 def get_indicies(str, ch):
     return [i for (i,c) in enumerate(str) if c == ch]
 
+def greatest_common_factor(a, b):
+    smaller = min(a, b)
+    larger = max(a, b)
+    if (smaller == 0):
+        return larger
+    return greatest_common_factor(smaller, larger % smaller)
 
-asteroid_map = reverse(sys.stdin)
+class Direction:
 
-coordinates = [(x,y) for (y, line) in enumerate(asteroid_map) for x in get_indicies(line, '#')]
+    def __init__(self, rise, run):
+        scale = greatest_common_factor(abs(rise), abs(run))
+        if (scale > 0):
+            rise /= scale
+            run /= scale
+        self.rise = rise
+        self.run = run
 
-for coord in coordinates:
-    print(coord)
+    @staticmethod
+    def from_coords(a, b):
+        rise = b[1] - a[1]
+        run = b[0] - a[0]
+        return Direction(rise, run)
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return False
+        return self.rise == other.rise and self.run == other.run
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __hash__(self):
+        return hash((self.rise, self.run))
+
+    def __repr__(self):
+       return 'Direction(%s, %s)' % (self.rise, self.run)
+
+    def __str__(self):
+       return '{ rise: %s, run: %s }' % (self.rise, self.run)
+
+
+coordinates = [(x,y) for (y, line) in enumerate(sys.stdin) for x in get_indicies(line, '#')]
+
+def get_paths_from(coord):
+    paths = defaultdict(list)
+    for other in coordinates:
+        if other == coord:
+            continue
+        paths[Direction.from_coords(coord, other)].append(other)
+    return paths
+
+path_sets = dict(map(lambda c: (c, get_paths_from(c)), coordinates))
+
+best_location = max(path_sets.items(), key=lambda t: len(t[1]))
+
+print('part 1:', best_location[0], len(best_location[1]))
